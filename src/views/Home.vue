a24b3d9c1fde46e6060faa1517b1d081a0bd261f
Connie Zhou <connie@Connies-MacBook-Pro-2.local>
2020-07-18 17:35:36 +0900

    msg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
+    <!-- You write html here. All content must be inside one tag.  -->
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <div>{{ connie }}</div>
+    <div>{{ stuffedAnimals[0] }}</div>
+
+    <button v-on:click="poo()">Pooh?</button>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
+
+  //data is where you store the data
+  data() {
+    return {
+      connie: "Zhou",
+      stuffedAnimals: ["pooh", "stitch", "toufu (goner)", "pink bear"],
+    };
+  },
+
+  //methods are where you write the functions
+  methods: {
+    poo() {
+      console.log("POOOOOOOOO!!!!!!!!");
+    },
+  },
+
+  //components
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
 };
 </script>
+
+<style>
+/* where you write CSS */
+</style>
